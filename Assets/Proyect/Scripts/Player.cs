c83c31cb2f339a55d3786e743f39d2ef910449de
SantiDEV2 <100314893+SantiDEV2@users.noreply.github.com>
2024-10-09 15:52:44 -0600

    FirstCommit

---
+++ Assets/Proyect/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player: MonoBehaviour 
+{
+    public int life = 15;
+    public float speed = 1f;
+
+    private void Update()
+    {
+    }
+}
